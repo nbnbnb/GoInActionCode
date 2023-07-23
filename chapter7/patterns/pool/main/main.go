04d8a3335e7c6f54cb252e899183c4b564ef3d00
JJZhang <dashan1010zj@163.com>
2023-07-23 19:14:53 +0800

    pool

--- chapter7/patterns/pool/main/main.go
+++ chapter7/patterns/pool/main/main.go
@@ -13,10 +13,17 @@ import (
 
 const (
 	// 要使用的 goroutine 的数量
-	maxGoroutines = 25
+	maxGoroutines = 100
 	// 池中的资源的数量
 	pooledResources = 2
 )
+
+// init 初始化包
+// Go 语言运行时会在其他代码执行之前优先执行这个函数
+func init() {
+	// 初始化随机数种子
+	rand.Seed(time.Now().Unix())
+}
 
 // dbConnection 模拟要共享的资源
 type dbConnection struct {
@@ -54,6 +61,15 @@ func main() {
 
 	// 使用池里的连接来完成查询
 	for query := 0; query < maxGoroutines; query++ {
+
+		// 添加一个等待时间
+		// 让 pool 中的 resources 可以被是否
+		// 用于模拟 Shared Resource 的效果
+
+		// 注意：不能放在 performQueries 中等待，因为这是一个闭包
+		// 全部请求会立即执行
+		time.Sleep(time.Duration(rand.Intn(1000)) * time.Microsecond)
+
 		// 每个 goroutine 需要自己复制一份要查询值的副本
 		// 不然所有的查询会共享同一个查询变量
 		go func(q int) {
@@ -65,8 +81,9 @@ func main() {
 	// 等待 goroutine 结束
 	wg.Wait()
 
-	// 关闭池
 	log.Println("Shutdown Program.")
+
+	// 关闭池
 	pool.Close()
 }
 
@@ -83,7 +100,7 @@ func performQueries(query int, pool *pool.Pool) {
 	defer pool.Release(conn)
 
 	// 用等待来模拟查询响应
-	time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+	time.Sleep(time.Duration(rand.Intn(1000)) * time.Microsecond)
 
 	log.Printf("Query: QID[%d] CID[%d]\n", query, conn.(*dbConnection).ID)
 }
--- chapter7/patterns/pool/pool.go
+++ chapter7/patterns/pool/pool.go
@@ -12,7 +12,8 @@ import (
 // 被管理的资源必须实现 io.Closer 接口
 type Pool struct {
 	// 这个互斥锁用来保证在多个 goroutine 访问资源池时，池内的值是安全的
-	mutex     sync.Mutex
+	mutex sync.Mutex
+	// 有缓冲的通道，用来管理资源的队列
 	resources chan io.Closer
 	factory   func() (io.Closer, error)
 	closed    bool
@@ -29,44 +30,45 @@ func New(fn func() (io.Closer, error), size uint) (*Pool, error) {
 	}
 
 	return &Pool{
-		factory:   fn,
+		factory: fn,
+		// 缓冲通道，大小为 size
 		resources: make(chan io.Closer, size),
 	}, nil
 }
 
 // Acquire 从池中获取一个资源
-func (p *Pool) Acquire() (io.Closer, error) {
+func (pool *Pool) Acquire() (io.Closer, error) {
 	select {
 	// 检查是否有空闲的资源
-	case r, ok := <-p.resources:
-		log.Println("Acquire:", "Shared Resource")
+	case resource, ok := <-pool.resources:
+		log.Println("Acquire:", "--------------------------- Shared Resource ---------------------------")
 		if !ok {
 			return nil, ErrPoolClosed
 		}
-		return r, nil
+		return resource, nil
 
 	// 因为没有空闲资源可用，所以提供一个新资源
 	default:
 		log.Println("Acquire:", "New Resource")
-		return p.factory()
+		return pool.factory()
 	}
 }
 
 // Release 将一个使用后的资源放回池里
-func (p *Pool) Release(r io.Closer) {
+func (pool *Pool) Release(r io.Closer) {
 	// 保证本操作和 Close 操作的安全
-	p.mutex.Lock()
-	defer p.mutex.Unlock()
+	pool.mutex.Lock()
+	defer pool.mutex.Unlock()
 
 	// 如果池已经被关闭，销毁这个资源
-	if p.closed {
+	if pool.closed {
 		r.Close()
 		return
 	}
 
 	select {
 	// 试图将这个资源放入队列
-	case p.resources <- r:
+	case pool.resources <- r:
 		log.Println("Release:", "In Queue")
 
 	// 如果队列已满，则关闭这个资源
@@ -77,25 +79,25 @@ func (p *Pool) Release(r io.Closer) {
 }
 
 // Close 会让资源池停止工作，并关闭所有现有的资源
-func (p *Pool) Close() {
+func (pool *Pool) Close() {
 	// 保证本操作与 Release 操作的安全
-	p.mutex.Lock()
-	defer p.mutex.Unlock()
+	pool.mutex.Lock()
+	defer pool.mutex.Unlock()
 
 	// 如果 pool 已经被关闭，什么也不做
-	if p.closed {
+	if pool.closed {
 		return
 	}
 
 	// 将池关闭
-	p.closed = true
+	pool.closed = true
 
 	// 在清空通道里的资源之前，将通道关闭
 	// 如果不这样做，会发生死锁
-	close(p.resources)
+	close(pool.resources)
 
 	// 关闭资源
-	for r := range p.resources {
-		r.Close()
+	for resource := range pool.resources {
+		resource.Close()
 	}
 }
